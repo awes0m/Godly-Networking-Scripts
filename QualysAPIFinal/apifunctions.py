b77f5fac961d89bffcc7cf887346aec13d45d89a
Som <50729508+awes0m@users.noreply.github.com>
2021-08-05 23:22:38 +0530

    Qualys api initial comit

---
+++ QualysAPIFinal/apifunctions.py
@@ -0,0 +1,119 @@
+import requests
+
+
+class ApiFunction:
+    USERNAME = "wamsn_qa1"
+    PASSWORD = "ruhn5rPWvsjGpJF"
+    endpoint_url = "https://qualysapi.qualys.com/api/2.0/fo/session/"
+    session_ID = None
+    unique_id = '1525419'
+
+    def __init__(self, username, password, uniqueid):
+        self.USERNAME = username,
+        self.PASSWORD = password,
+        self.unique_id = uniqueid,
+
+    # Update Host Asset-(Done)
+    def add_ip(self):
+        USERNAME = self.USERNAME,
+        PASSWORD = self.PASSWORD,
+        headers = {
+            'X-Requested-With': 'Curl',
+            'Content-Type': 'text/csv',
+        }
+
+        params = (
+            ('action', 'add'),
+            ('enable_vm', '1'),
+            ('enable_pc', '1'),
+            ('tracking_method', 'IP'),
+        )
+        data = open('ips_list.csv', 'rb').read()  # -----------------------TODO Create file- (not done)
+        response = requests.post('https://qualysapi.qualys.com/api/2.0/fo/asset/ip/', headers=headers, params=params,
+                                 data=data, auth=(f'{USERNAME}', f'{PASSWORD}'))
+
+        with open('Hostassetlog.xml', 'w') as f:
+            f.write(f'str({response.text})')
+            f.write(f'str({response.headers})')
+
+    # Update Asset Group-(Done)
+    def clear_asset_group(self):
+        USERNAME = self.USERNAME,
+        PASSWORD = self.PASSWORD,
+        unique_id = self.unique_id,
+        headers = {
+            'X-Requested-With': 'Curl',
+        }
+
+        params = (
+            ('action', 'edit'),
+        )
+
+        dataA = {
+            'id': unique_id,
+            'remove_ips': '10.0.0.0-10.255.255.255',
+        }
+        dataB = {
+            'id': unique_id,
+            'remove_ips': '128.0.0.0-192.0.0.255',
+        }
+
+        responseA = requests.post('https://qualysapi.qualys.com/api/2.0/fo/asset/group/', headers=headers,
+                                  params=params,
+                                  data=dataA, auth=(f'{USERNAME}', f'{PASSWORD}'))
+        responseB = requests.post('https://qualysapi.qualys.com/api/2.0/fo/asset/group/', headers=headers,
+                                  params=params,
+                                  data=dataB, auth=(f'{USERNAME}', f'{PASSWORD}'))
+        with open('Assetgroupdeletionlog.xml', 'w') as f:
+            f.write(f'str({responseA.text})')
+            f.write(f'str({responseA.headers})')
+            f.write(f'str({responseB.text})')
+            f.write(f'str({responseB.headers})')
+
+    def update_Asset_group(self):
+        USERNAME = self.USERNAME,
+        PASSWORD = self.PASSWORD,
+        unique_id = self.unique_id,
+        headers = {
+            'X-Requested-With': 'Curl',
+        }
+
+        params = (
+            ('action', 'edit'),
+        )
+        iplist = open('ips_list.csv', 'rb').read()
+
+        data = {
+            'id': unique_id,
+            'add_ips': iplist,
+        }
+
+        response = requests.post('https://qualysapi.qualys.com/api/2.0/fo/asset/group/', headers=headers, params=params,
+                                 data=data, auth=(f'{USERNAME}', f'{PASSWORD}'))
+        with open('Assetgrouplog.xml', 'w') as f:
+            f.write(f'str({response.text})')
+            f.write(f'str({response.headers})')
+
+    #TODO 4-Update Authentication--(Not tested)
+    def auth_add(self):
+        USERNAME = self.USERNAME,
+        PASSWORD = self.PASSWORD,
+        unique_id = self.unique_id,
+        headers = {
+            'X-Requested-With': 'curl',
+        }
+        iplist = open('ips_list.csv', 'rb').read()
+
+        data = {
+            'add_ips': iplist,
+        }
+
+        params = {
+            'action':'update',
+            'id':f'{unique_id}',
+        }
+
+        response = requests.get('https://qualysapi.qualys.com/api/2.0/fo/auth/windows/', headers=headers, params=params,data=data,
+                                auth=(f'{USERNAME}', f'{PASSWORD}'))
+        print(response.text)
+        print(response.headers)
---
+++ QualysAPIFinal/main.py
@@ -0,0 +1,85 @@
+
+from tkinter import messagebox
+from tkinter import *
+import os
+
+from requests import NullHandler
+
+
+from apifunctions import ApiFunction
+
+#-----------------------------Global Constants and Initializers---------------------------------#
+USERNAME = "wamsn_qa1"
+PASSWORD = "ruhn5rPWvsjGpJF"
+endpoint_url ="https://qualysapi.qualys.com/api/2.0/fo/session/"
+session_ID=None
+uniqueid='1525419'
+
+
+
+#-----------------------------------------Logic-----------------------------------------------#
+
+def open_file():
+    f= open("ips_list.csv","w+")
+    f.close
+    os.system("open -a 'path/Microsoft Excel.app' 'ips_list.csv'")
+
+def submitted():
+    messagebox.showinfo(
+        title='Information',
+        message='Username and Password Submitted!'
+    )
+    confirm.config(state=DISABLED)
+    reset_button.config(state=NORMAL)
+def reset():
+    if username.get().isEmpty():
+        messagebox.showinfo(
+            title='Information',
+            message='Username and Password Submitted!'
+    )
+    confirm.config(state=NORMAL)
+    reset_button.config(state=DISABLED)
+
+# af=ApiFunction(USERNAME,PASSWORD,uniqueid),
+
+# af.clear_asset_group(),
+# af.update_Asset_group(),
+
+
+
+
+#-----------------------------------------UI-----------------------------------------------#
+window=Tk()
+window.title("Api upload")
+window.minsize(width=400,height=200)
+#Entries
+Usernamelabel = Label(text="Username")
+Passwordlabel=Label(text="Password")
+username=Entry(width=30)
+username.focus()
+password=Entry(width=30)
+uniqueid=Entry(width=30)
+confirm=Button(text="Submit and Lock",command=submitted)
+reset_button=Button(text="Submit and Lock",command=reset)
+reset_button.config(state=DISABLED)
+
+
+OpenFile=Button(text="Open IP List",command=NullHandler)
+Assetgroup_update=Button(text="Update Asset",command=NullHandler)
+
+openfile=Button(command=open_file)
+openfile.config(text="open file")
+
+#------------------------------Grid layout
+Usernamelabel.grid(column=1,row=1)
+Passwordlabel.grid(column=1,row=2)
+username.grid(column=2,row=1)
+password.grid(column=2,row=2)
+
+
+Assetgroup_update.grid(column=2,row=4)
+openfile.grid(column=3,row=4)
+
+
+window.mainloop()
+#-----------------------------------------xx----------------------------------------------------
