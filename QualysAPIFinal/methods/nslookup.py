4e7f21aeaf9945106ac0c5aeeaa7e9fa48677464
Som <50729508+awes0m@users.noreply.github.com>
2021-08-20 07:12:06 +0000

    using subprocess

--- QualysAPIFinal/methods/nslookup.py
+++ QualysAPIFinal/methods/nslookup.py
@@ -1,8 +1,21 @@
-import socket
+import subprocess
 
-# host to ip
-def hostToip(hostname="localhost"):
-    return socket.gethostbyname(hostname)
+process = subprocess.Popen(["nslookup", "www.google.com"], stdout=subprocess.PIPE)
+output = process.communicate()[0].split('\n')
+
+ip_arr = []
+for data in output:
+    if 'Address' in data:
+        ip_arr.append(data.replace('Address: ',''))
+ip_arr.pop(0)
+
+print ip_arr
+
+# import socket
+
+# # host to ip
+# def hostToip(hostname="localhost"):
+#     return socket.gethostbyname(hostname)
 
  # ip to host
 
