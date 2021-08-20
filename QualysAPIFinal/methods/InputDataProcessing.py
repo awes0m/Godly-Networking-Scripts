544bfe41dc0a5883303a1a73b2b225a7406f45d8
awes0m <somsubhra.pandit@gmail.com>
2021-08-20 06:34:39 -0500

    ips generated, errors caught,ip list Sorted

--- QualysAPIFinal/methods/InputDataProcessing.py
+++ QualysAPIFinal/methods/InputDataProcessing.py
@@ -1,18 +1,35 @@
-
-
-from nslookup import hostToip
 import os
 import socket
 
-
-
-# host to ip
-
+#globalConstants
 data=[]
-k=0
-with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\ip_list.txt", 'rb') as f:
-    data=[i.decode().split('.')[0].upper() for i in f]        
-f.close
+ipData=[]
 
-for i in data:
-    print(hostToip(i))
+      
+def hosttoip(hostdata):
+    global ipData
+    if os.path.exists(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv"):
+        os.remove(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv")
+    else:
+        with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv", 'w+')as errfile:
+            for i in hostdata:
+            
+                try:
+                    print(f"Processing ip {str(socket.gethostbyname_ex(i)[2])}")
+                    ipData.append(str(socket.gethostbyname_ex(i)[2][0]))
+                except Exception as err:
+                    print(f"Error Processing {i} , Check error log for more(ipconvert_errorlog.csv)")
+                    errfile.write(f"{i},{err} \n")
+        print ("All (possible) Hosts converted to ip")
+
+
+def IPrangeConverter(iplist):
+    # ipSplit=[i.split(".") for i in iplist]
+    for ip in sorted(iplist, key = lambda ip: [int(ip) for ip in ip.split(".")] ):
+            print(ip)
+
+
+with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\test.txt", 'rb') as f:
+    data=[i.decode().split('.')[0].upper() for i in f] 
+hosttoip(data)
+IPrangeConverter(ipData)
--- QualysAPIFinal/methods/nslookup.py
+++ QualysAPIFinal/methods/nslookup.py
@@ -1,15 +1,27 @@
 import subprocess
 
-process = subprocess.Popen(["nslookup", "www.google.com"], stdout=subprocess.PIPE)
-output = process.communicate()[0].split('\n')
+# Takes an Ip adress or Hostname 
+# and returns an string arrray [HOSTNAME,IP_adress]
+#to get host Nslookup(data)[0]
+#to get IP_adrr Nslookup(data)[1]
+        
+def NSLookup(Host_ip='localhost'):
+    process = subprocess.Popen(["nslookup", f"{Host_ip}"], stdout=subprocess.PIPE)
 
-ip_arr = []
-for data in output:
-    if 'Address' in data:
-        ip_arr.append(data.replace('Address: ',''))
-ip_arr.pop(0)
+    output = process.communicate()[0].decode().split('\n')
 
-print ip_arr
+    host_arr = []
+    for data in output:
+        if 'Name' in data:
+            host_arr.append(data.replace('Name: ',''))
+            
+    ip_arr = []
+    for data in output:
+        if 'Address' in data:  
+            ip_arr.append(data.replace('Address: ',''))
+            
+    ip_arr.pop(0)
+    return [str(host_arr[0]).strip(),str(ip_arr[0]).strip()]
 
 # import socket
 
@@ -19,6 +31,6 @@ print ip_arr
 
  # ip to host
 
-def ipTohost(ip="127.0.0.0"):
-    hostName = socket.gethostbyaddr(ip)
-    return hostName[0]
+# def ipTohost(ip="127.0.0.0"):
+#     hostName = socket.gethostbyaddr(ip)
+#     return hostName[0]
