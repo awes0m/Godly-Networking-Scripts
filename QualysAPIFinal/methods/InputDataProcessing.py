c50eda8937976ef8a2f8a854e2fa4b74b0429743
awes0m <somsubhra.pandit@gmail.com>
2021-08-23 04:11:35 -0500

    fixed and converted ips to integers

--- QualysAPIFinal/methods/InputDataProcessing.py
+++ QualysAPIFinal/methods/InputDataProcessing.py
@@ -1,35 +1,65 @@
 import os
 import socket
 
-#globalConstants
-data=[]
-ipData=[]
 
-      
 def hosttoip(hostdata):
-    global ipData
+    ipData = []
     if os.path.exists(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv"):
         os.remove(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv")
-    else:
-        with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv", 'w+')as errfile:
-            for i in hostdata:
-            
-                try:
-                    print(f"Processing ip {str(socket.gethostbyname_ex(i)[2])}")
-                    ipData.append(str(socket.gethostbyname_ex(i)[2][0]))
-                except Exception as err:
-                    print(f"Error Processing {i} , Check error log for more(ipconvert_errorlog.csv)")
-                    errfile.write(f"{i},{err} \n")
-        print ("All (possible) Hosts converted to ip")
+        f = open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv", 'w+')
+        f.close()
+
+    with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\QualysAPIFinal\methods\Logs\ipconvert_errorlog.csv", 'w+')as errfile:
+        for i in hostdata:
+
+            try:
+                print(f"Processing ip {str(socket.gethostbyname_ex(i)[2])}")
+                ipData.append(str(socket.gethostbyname_ex(i)[2][0]))
+            except Exception as err:
+                print(
+                    f"Error Processing {i} , Check error log for more(ipconvert_errorlog.csv)")
+                errfile.write(f"{i},{err} \n")
+        print("All (possible) Hosts converted to ip")
+    return ipData
 
 
-def IPrangeConverter(iplist):
-    # ipSplit=[i.split(".") for i in iplist]
-    for ip in sorted(iplist, key = lambda ip: [int(ip) for ip in ip.split(".")] ):
-            print(ip)
+def IPSorter(iplist):
+    return [
+        ip
+        for ip in sorted(
+            iplist, key=lambda ip: [int(ip) for ip in ip.split(".")]
+        )
+    ]
 
 
-with open(r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\test.txt", 'rb') as f:
-    data=[i.decode().split('.')[0].upper() for i in f] 
-hosttoip(data)
-IPrangeConverter(ipData)
+def listToString(s): 
+    return "".join(str(each) for each in s)
+def IpToQualysdata(sortedIPData=[]):
+    for _ in range(len(sortedIPData)-1):
+        ip = sortedIPData[_]
+        IPnumber = int(listToString([int(ip) for ip in ip.split(".")]))
+        nxtip=sortedIPData[_+1]
+        nxtIPnumber = int(listToString([int(nxtip) for nxtip in nxtip.split(".")]))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+def filetoHostList(file=r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\test.txt"):
+    with open(file, 'rb') as f:
+        return [i.decode().split('.')[0].upper() for i in f]
+
+
+filepath = r"C:\Users\ssubhra\Desktop\Git repositories\Godly-Networking-Scripts\ip_list.csv"
+data = filetoHostList()
+ipData = hosttoip(data)
+sortedIPData = IPSorter(ipData)
+IpToQualysdata(sortedIPData)
